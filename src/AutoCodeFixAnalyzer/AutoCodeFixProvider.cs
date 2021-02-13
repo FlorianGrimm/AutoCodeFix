85a5dc01d5c1f45a382f8dce6b0661f06b01a8db
FlorianGrimm <grimmuko@googlemail.com>
2021-02-13 15:48:19 +0100

    step

---
+++ src/AutoCodeFixAnalyzer/AutoCodeFixProvider.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.CodeAnalysis.CodeFixes;
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoCodeFixAnalyzer {
+    public interface IAutoCodeFixProvider {
+    }
+    public class AutoCodeFixProvider : CodeFixProvider, IAutoCodeFixProvider {
+        public override ImmutableArray<string> FixableDiagnosticIds => throw new NotImplementedException();
+        public override Task RegisterCodeFixesAsync(CodeFixContext context) {
+            throw new NotImplementedException();
+        }
+        public override FixAllProvider? GetFixAllProvider() {
+            return base.GetFixAllProvider();
+        }
+    }
+}
--- src/AutoCodeFixer/Program.cs
+++ src/AutoCodeFixer/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+using System;
+using System.Collections.Generic;
 using System.CommandLine;
 using System.CommandLine.Invocation;
 using System.IO;
@@ -51,24 +55,90 @@ namespace AutoCodeFixer {
         }
         public static async Task<int> RunAsync(FileInfo? fileSolution, FileInfo? fileProject, CancellationToken cancellationToken) {
             if (fileSolution is null && fileProject is null) {
-                var lstsln = System.IO.Directory.EnumerateFiles(System.Environment.CurrentDirectory, ".sln").ToList();
-                var lstcsprj = System.IO.Directory.EnumerateFiles(System.Environment.CurrentDirectory, ".csproj").ToList();
+                string searchPath = System.Environment.CurrentDirectory;
+                var lstsln = System.IO.Directory.EnumerateFiles(searchPath, ".sln").ToList();
+                var lstcsprj = System.IO.Directory.EnumerateFiles(searchPath, ".csproj").ToList();
                 if (lstsln.Count == 1) {
+                    fileSolution = new FileInfo(lstcsprj[0]);
                 } else if (lstsln.Count > 1) {
+                    await System.Console.Error.WriteLineAsync($"{lstsln.Count} solutions found. Plese specify.");
+                    return 1;
                 } else {
                     if (lstcsprj.Count == 1) {
+                        fileProject = new FileInfo(lstcsprj[0]);
                     } else if (lstcsprj.Count > 1) {
+                        await System.Console.Error.WriteLineAsync($"{lstsln.Count} (cs) projects found. Plese specify.");
+                        return 1;
                     } else {
-                        System.Console.Error.WriteLine("Not found");
+                        await System.Console.Error.WriteLineAsync($"No solution or project found ín {searchPath}.");
                         return 1;
                     }
                 }
+            }
 
+            {
+                using var workspace = AnalyzerRunnerHelper.CreateWorkspace();
+                Solution? solution = null;
+                Project? project = null;
+                var lstAllProjectId = new List<ProjectId>();
+                var lstFilteredProjectId = new List<ProjectId>();
+
+                if (fileSolution is object) {
+                    if (!fileSolution.Exists) {
+                        await System.Console.Error.WriteLineAsync($"Solution {fileSolution.FullName} does not exists.");
+                        return 1;
+                    }
+                    solution = await workspace.OpenSolutionAsync(fileSolution.FullName, progress: null, cancellationToken).ConfigureAwait(false);
+                    if (fileProject is object) {
+                        var fullNameProject = fileProject.FullName;
+                        var lstProjects = solution.Projects.Where(prj => string.Equals(prj.FilePath, fullNameProject, StringComparison.OrdinalIgnoreCase)).ToList();
+                        if (lstProjects.Count == 1) {
+                            project = lstProjects[0];
+                            lstFilteredProjectId.Add(project.Id);
+                        } else if (lstProjects.Count > 1) {
+                            await System.Console.Error.WriteLineAsync($"For file {fileProject.FullName} found {lstProjects.Count} projects ?? Unexpected.");
+                            return 1;
+                        } else {
+                            await System.Console.Error.WriteLineAsync($"For file {fileProject.FullName} no project found.");
+                            return 1;
+                        }
+                    }
+                    lstAllProjectId.AddRange(solution.GetProjectDependencyGraph().GetTopologicallySortedProjects(cancellationToken));
+                } else if (fileProject is object) {
+                    if (!fileProject.Exists) {
+                        await System.Console.Error.WriteLineAsync($"Project {fileProject.FullName} does not exists.");
+                        return 1;
+                    }
+                    project = await workspace.OpenProjectAsync(fileProject.FullName, null, cancellationToken).ConfigureAwait(false);
+                    solution = project.Solution;
+                    lstAllProjectId.Add(project.Id);
+                    lstFilteredProjectId.Add(project.Id);
+                } else {
+                    await System.Console.Error.WriteLineAsync($"No solution nor project loaded.");
+                    return 1;
+                }
+                await RunLoadAnalzerAsync(solution, project, lstFilteredProjectId, lstAllProjectId);
+            }
+            return 0;
+        }
+
+        private static async Task RunLoadAnalzerAsync(
+                Solution solution,
+                Project? project,
+                List<ProjectId> lstFilteredProjectId,
+                List<ProjectId> lstAllProjectId
+            ) {
+            foreach (var projectId in lstFilteredProjectId) {
+                var prj = solution.GetProject(projectId);
+                if (prj is null) { continue; }
+                foreach (var analyzerReference in prj.AnalyzerReferences) {
+                    var analyzers = analyzerReference.GetAnalyzers(prj.Language);
+                    foreach(var analyzer in analyzers) {
+
+                    }
+                }
             }
             await Task.Yield();
-            Console.WriteLine($"The value for --solution is: {fileSolution?.FullName ?? "null"}");
-            Console.WriteLine($"The value for --project is: {fileProject?.FullName ?? "null"}");
-            return 0;
         }
     }
 }
